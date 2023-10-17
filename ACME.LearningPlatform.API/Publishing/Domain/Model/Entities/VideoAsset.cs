5fa310f2f765bba5648f33c88b2ce4830714418e
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-16 19:47:09 -0500

    feat(video-asset): added video asset.

---
+++ ACME.LearningPlatform.API/Publishing/Domain/Model/Entities/VideoAsset.cs
@@ -0,0 +1,21 @@
+using ACME.LearningPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningPlatform.API.Publishing.Domain.Model.Entities;
+
+public class VideoAsset: Asset
+{
+    private Uri VideoUri { get; }
+    
+    public VideoAsset(string videoUrl) : base(EAssetType.Video)
+    {
+        VideoUri = new Uri(videoUrl);        
+    }
+
+    public override string GetContent()
+    {
+        return VideoUri.AbsoluteUri;
+    }
+
+    public override bool Readable => false;
+    public override bool Viewable => true;
+}
