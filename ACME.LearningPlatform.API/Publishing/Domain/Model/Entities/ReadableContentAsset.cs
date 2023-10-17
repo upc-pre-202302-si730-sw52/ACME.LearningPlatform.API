9fae62b1a0c5da2c532996727ef10de3d4ec78ac
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-16 19:43:23 -0500

    feat(readable-content-asset): added readable content asset.

---
+++ ACME.LearningPlatform.API/Publishing/Domain/Model/Entities/ReadableContentAsset.cs
@@ -0,0 +1,21 @@
+using ACME.LearningPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningPlatform.API.Publishing.Domain.Model.Entities;
+
+public class ReadableContentAsset: Asset
+{
+    private string ReadableContent { get; set; }
+    
+    public ReadableContentAsset(string content) : base(EAssetType.ReadableContentItem)
+    {
+        ReadableContent = content;
+    }
+
+    public override string GetContent()
+    {
+        return ReadableContent;
+    }
+
+    public override bool Readable => true;
+    public override bool Viewable => false;
+}
