f891153e9b8b6d8b4e3b51abc6189e5d7e182312
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-18 20:19:34 -0500

    chore: added dependency injection configuration.

--- ACME.LearningPlatform.API/Program.cs
+++ ACME.LearningPlatform.API/Program.cs
@@ -1,3 +1,13 @@
+using ACME.LearningPlatform.API.Publishing.Application.Internal.CommandServices;
+using ACME.LearningPlatform.API.Publishing.Application.Internal.QueryServices;
+using ACME.LearningPlatform.API.Publishing.Domain.Repositories;
+using ACME.LearningPlatform.API.Publishing.Domain.Services;
+using ACME.LearningPlatform.API.Publishing.Infrastructure.Repositories;
+using ACME.LearningPlatform.API.Shared.Domain.Repositories;
+using ACME.LearningPlatform.API.Shared.Infrastructure.Configuration;
+using ACME.LearningPlatform.API.Shared.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,42 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Database Connection
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Database Context and Logging Levels
+
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (connectionString != null)
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+    });
+
+// Configure Lowercase URLs
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Configure Dependency Injection
+
+// Shared Bounded Context Injection Configuration
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Publishing Bounded Context Injection Configuration
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryCommandService, CategoryCommandService>();
+builder.Services.AddScoped<ICategoryQueryService, CategoryQueryService>();
+builder.Services.AddScoped<ITutorialRepository, TutorialRepository>();
+builder.Services.AddScoped<ITutorialCommandService, TutorialCommandService>();
+builder.Services.AddScoped<ITutorialQueryService, TutorialQueryService>();
+
 
 var app = builder.Build();
 
