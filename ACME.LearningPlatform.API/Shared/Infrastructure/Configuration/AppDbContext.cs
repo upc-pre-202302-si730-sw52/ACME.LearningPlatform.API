5bbbb9add679a059c25b54294fef26ddc4d73293
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-18 19:58:34 -0500

    chore: added entities initial configuration.

--- ACME.LearningPlatform.API/Shared/Infrastructure/Configuration/AppDbContext.cs
+++ ACME.LearningPlatform.API/Shared/Infrastructure/Configuration/AppDbContext.cs
@@ -1,3 +1,6 @@
+using ACME.LearningPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningPlatform.API.Publishing.Domain.Model.Entities;
+using ACME.LearningPlatform.API.Shared.Infrastructure.Configuration.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace ACME.LearningPlatform.API.Shared.Infrastructure.Configuration;
@@ -12,6 +15,22 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        // Category Entity Configuration
+        builder.Entity<Category>().ToTable("Categories");
+        builder.Entity<Category>().HasKey(c => c.Id);
+        builder.Entity<Category>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Category>().Property(c => c.Name).IsRequired().HasMaxLength(30);
+        
+        // Tutorial Entity Configuration
+        builder.Entity<Tutorial>().ToTable("Tutorials");
+        builder.Entity<Tutorial>().HasKey(t => t.Id);
+        builder.Entity<Tutorial>().Property(t => t.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Tutorial>().Property(t => t.Title).IsRequired().HasMaxLength(50);
+        builder.Entity<Tutorial>().Property(t => t.Summary).IsRequired().HasMaxLength(240);
+        
+        // Apply snake case naming convention
+        builder.UseSnakeCaseNamingConvention();
         
         
     }
