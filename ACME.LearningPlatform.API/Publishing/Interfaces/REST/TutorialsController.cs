21b3c018b1252d3d8ba2dc2e34c9d32913500894
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-25 19:57:57 -0500

    feat(tutorials): added tutorial inbound service.

---
+++ ACME.LearningPlatform.API/Publishing/Interfaces/REST/TutorialsController.cs
@@ -0,0 +1,42 @@
+using ACME.LearningPlatform.API.Publishing.Domain.Model.Queries;
+using ACME.LearningPlatform.API.Publishing.Domain.Services;
+using ACME.LearningPlatform.API.Publishing.Interfaces.REST.Resources;
+using ACME.LearningPlatform.API.Publishing.Interfaces.Transform;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACME.LearningPlatform.API.Publishing.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class TutorialsController : ControllerBase
+{
+    private readonly ITutorialCommandService _tutorialCommandService;
+    private readonly ITutorialQueryService _tutorialQueryService;
+
+    public TutorialsController(ITutorialCommandService tutorialCommandService, ITutorialQueryService tutorialQueryService)
+    {
+        _tutorialCommandService = tutorialCommandService;
+        _tutorialQueryService = tutorialQueryService;
+    }
+
+    [HttpGet("{tutorialIdentifier}")]
+    public async Task<IActionResult> GetTutorialByIdentifier([FromRoute] int tutorialIdentifier)
+    {
+        var tutorial = await _tutorialQueryService.Handle(new GetTutorialByIdentifierQuery(tutorialIdentifier));
+        if (tutorial == null) return NotFound();
+        var resource = TutorialResourceFromEntityAssembler.ToResourceFromEntity(tutorial);
+        return Ok(resource);
+    }
+
+    [HttpPost("{tutorialIdentifier}/videos")]
+    public async Task<IActionResult> AddVideoToTutorial([FromBody] AddVideoAssetResource addVideoAssetResource,
+        [FromRoute] int tutorialIdentifier)
+    {
+        var addVideAssetToTutorialCommand =
+            AddVideoAssetToTutorialCommandFromResourceAssembler.ToCommandFromResource(addVideoAssetResource,
+                tutorialIdentifier);
+        var tutorial = await _tutorialCommandService.Handle(addVideAssetToTutorialCommand);
+        var resource = TutorialResourceFromEntityAssembler.ToResourceFromEntity(tutorial);
+        return CreatedAtAction(nameof(GetTutorialByIdentifier), new { tutorialIdentifier = resource.Id }, resource);
+    }
+}
