309d80948b7c56027c00bcc69d40b0e4be664aa3
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-30 20:20:04 -0500

    docs: added documentation for categories inbound service.

--- ACME.LearningPlatform.API/Publishing/Interfaces/REST/CategoriesController.cs
+++ ACME.LearningPlatform.API/Publishing/Interfaces/REST/CategoriesController.cs
@@ -1,13 +1,23 @@
+using System.Net.Mime;
 using ACME.LearningPlatform.API.Publishing.Domain.Model.Queries;
 using ACME.LearningPlatform.API.Publishing.Domain.Services;
 using ACME.LearningPlatform.API.Publishing.Interfaces.REST.Resources;
 using ACME.LearningPlatform.API.Publishing.Interfaces.REST.Transform;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace ACME.LearningPlatform.API.Publishing.Interfaces.REST;
 
+/**
+ * Categories Controller
+ * <summary>
+ *    This controller is responsible for handling all the requests related to categories.
+ * </summary>
+ */
+
 [ApiController]
 [Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
 public class CategoriesController: ControllerBase
 {
     private readonly ICategoryCommandService _categoryCommandService;
@@ -19,7 +29,20 @@ public class CategoriesController: ControllerBase
         _categoryQueryService = categoryQueryService;
     }
 
+    /**
+     * Create Category.
+     * <summary>
+     *  This method is responsible for handling the request to create a new category.
+     * </summary>
+     * <param name="createCategoryResource">The resource containing the information to create a new category.</param>
+     * <returns>The newly created category resource.</returns>
+     */
     [HttpPost]
+    [SwaggerOperation(
+        Summary = "Creates a category", 
+        Description = "Creates a category with a given name",
+        OperationId = "CreateCategory")]
+    [SwaggerResponse(201, "The category was created", typeof(CategoryResource))]
     public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryResource createCategoryResource)
     {
         var createCategoryCommand = CreateCategoryCommandFromResourceAssembler.ToCommandFromResource(createCategoryResource);
@@ -28,7 +51,20 @@ public class CategoriesController: ControllerBase
         return CreatedAtAction(nameof(GetCategoryById), new { id = resource.Id }, resource);
     }
 
+    /**
+     * Get Category By Id.
+     * <summary>
+     *  This method is responsible for handling the request to get a category by its id.
+     * </summary>
+     * <param name="id">The category identifier.</param>
+     * <returns>The category resource.</returns>
+     */
     [HttpGet("{id:int}")]
+    [SwaggerOperation(
+        Summary = "Gets a category by id", 
+        Description = "Gets a category for a given identifier",
+        OperationId = "GetCategoryById")]
+    [SwaggerResponse(200, "The category was found", typeof(CategoryResource))]
     public async Task<IActionResult> GetCategoryById(int id)
     {
         var getCategoryByIdQuery = new GetCategoryByIdQuery(id);
