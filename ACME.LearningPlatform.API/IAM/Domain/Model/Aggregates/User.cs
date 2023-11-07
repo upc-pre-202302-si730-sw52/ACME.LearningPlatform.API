c32ca525d7c443de7021e7e6256eb5dd25dcb4f5
Angel Velasquez <pcisavel@upc.edu.pe>
2023-11-06 19:47:29 -0500

    feat(users): added user aggregate.

---
+++ ACME.LearningPlatform.API/IAM/Domain/Model/Aggregates/User.cs
@@ -0,0 +1,57 @@
+using System.Text.Json.Serialization;
+
+namespace ACME.LearningPlatform.API.IAM.Domain.Model.Aggregates;
+
+/**
+ * <summary>
+ *     The User Aggregate.
+ * </summary>
+ * <remarks>
+ *     This class is used to represent a user in the system.
+ * </remarks>
+ */
+public class User
+{
+    public User(string username, string passwordHash)
+    {
+        Username = username;
+        PasswordHash = passwordHash;
+    }
+
+    public User()
+    {
+        Username = string.Empty;
+        PasswordHash = string.Empty;
+    }
+
+    public int Id { get; private set; }
+    public string Username { get; private set; }
+    [JsonIgnore]
+    public string PasswordHash { get; private set; }
+    
+    /**
+     * <summary>
+     *     Updates the username of the user.
+     * </summary>
+     * <param name="username">The new username.</param>
+     * <returns>The updated user.</returns>
+     */
+    public User UpdateUsername(string username)
+    {
+        Username = username;
+        return this;
+    }
+    
+    /**
+     * <summary>
+     *     Updates the password hash of the user.
+     * </summary>
+     * <param name="passwordHash">The new password hash.</param>
+     * <returns>The updated user.</returns>
+     */
+    public User UpdatePasswordHash(string passwordHash)
+    {
+        PasswordHash = passwordHash;
+        return this;
+    }
+}
