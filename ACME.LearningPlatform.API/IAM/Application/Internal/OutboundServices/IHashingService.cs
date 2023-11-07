adf6b51e12e774668873d7c9b91dad0e1ba19217
Angel Velasquez <pcisavel@upc.edu.pe>
2023-11-06 20:24:44 -0500

    docs: added code documentation for hashing service.

--- ACME.LearningPlatform.API/IAM/Application/Internal/OutboundServices/IHashingService.cs
+++ ACME.LearningPlatform.API/IAM/Application/Internal/OutboundServices/IHashingService.cs
@@ -1,7 +1,25 @@
 namespace ACME.LearningPlatform.API.IAM.Application.Internal.OutboundServices;
 
+/**
+ * Hashing service.
+ * <summary>
+ * This interface is used to hash passwords and verify them.
+ * </summary>
+ */
 public interface IHashingService
 {
+    /**
+     * Hashes a password.
+     * <param name="password">The original password</param>
+     * <returns>The hashed password</returns>
+     */
     string HashPassword(string password);
+    
+    /**
+     * Verifies a password.
+     * <param name="password">The original password</param>
+     * <param name="passwordHash">The hashed password</param>
+     * <returns>True if the password is valid, false otherwise</returns>
+     */
     bool VerifyPassword(string password, string passwordHash);
 }
--- ACME.LearningPlatform.API/IAM/Infrastructure/Hashing/Services/HashingService.cs
+++ ACME.LearningPlatform.API/IAM/Infrastructure/Hashing/Services/HashingService.cs
@@ -3,13 +3,30 @@ using BCryptNet = BCrypt.Net.BCrypt;
 
 namespace ACME.LearningPlatform.API.IAM.Infrastructure.Hashing.Services;
 
+/**
+ * Hashing service.
+ * <summary>
+ * This class is used to hash passwords and verify them.
+ * </summary>
+ */
 public class HashingService : IHashingService
 {
+    /**
+     * Hashes a password.
+     * <param name="password">The original password</param>
+     * <returns>The hashed password</returns>
+     */
     public string HashPassword(string password)
     {
         return BCryptNet.HashPassword(password);
     }
 
+    /**
+     * Verifies a password.
+     * <param name="password">The original password</param>
+     * <param name="passwordHash">The hashed password</param>
+     * <returns>True if the password is valid, false otherwise</returns>
+     */
     public bool VerifyPassword(string password, string passwordHash)
     {
         return BCryptNet.Verify(password, passwordHash);
